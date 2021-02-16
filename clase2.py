9e40e097ae8da3ed75706d3d6f9d07a3ad4fd126
mariapaula017 <mariapaulasuarezv@gmail.com>
2021-02-16 00:55:35 -0500

    git add

---
+++ clase2.py
@@ -0,0 +1,27 @@
+pruebaV = True 
+pruebaF = False
+print("pruebaF")
+print("pruebaV")
+edad = 20
+estatura = 1.55
+peso = 57
+NOMBRE = "Maria paula suarez v"
+print("#"*15,"Mayor Edad", "#"*15)
+ismayorEdad = edad>= 18
+print(ismayorEdad)
+print("#"*15,"Bajo la Estatura promedio", "#"*15)
+isMayorEstatura = estatura < 1.70
+print(isMayorEstatura)
+# calculando peso diferente 57
+print("#"*15, "peso diferente 57", "#"*15)
+isPesoDiferente = peso != 57
+print(isPesoDiferente)
+
+
+a=12
+b=3
+
+C=b+a
+print(C)
+
+
