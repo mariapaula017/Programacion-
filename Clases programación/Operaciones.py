ade3e16eb9f91aa5fb0c7266c79cc1a65260d40f
mariapaula017 <mariapaulasuarezv@gmail.com>
2021-02-22 10:17:04 -0500

    subiendo clase

--- Clases programación/Operaciones.py
+++ Clases programación/Operaciones.py
@@ -2,12 +2,12 @@ numeroA = 87
 numeroB = 83
 sumar = numeroA + numeroB
 print ("el resultado de la suma es", sumar)
-print (f"la suma dio {sumar exitosamente")
+print (f"la suma dio {sumar} exitosamente")
 restar = numeroA - numeroB
-print(f"la resta dio {restar exitosamente")
+print(f"la resta dio {restar} exitosamente")
 multiplicar = numeroA * numeroB 
-print(f"la multiplicacion dio {multiplicar exitosamente")
+print(f"la multiplicacion dio {multiplicar} exitosamente")
 dividir = numeroA / numeroB
-print(f"la division dio {dividir exitosamente")
-exponete = numeroA**numeroB
-print(f"la exponente dio {exponente exitosamente")
+print(f"la division dio {dividir} exitosamente")
+exponente = numeroA**numeroB
+print(f"la exponente dio {exponente} exitosamente")
